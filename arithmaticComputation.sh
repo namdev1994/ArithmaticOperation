2eb502f8c376f2ad29ea17c62df79a1739185b1a
namdev1994 <n.lavhare1994@gmail.com>
2021-03-30 12:42:03 +0530

    Added:Arithmetic operation using Dictionary

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -1,3 +1,5 @@
+declare -A arithmaticOperation
+
 read -p "Enter the frist input :" fristInput
 read -p "Enter the second input :" secondInput
 read -p "Enter the third input :" thirdInput
@@ -7,3 +9,11 @@ result1=$(($fristInput*$secondInput+$thirdInput))
 echo $result1
 result2=$(($thirdInput+$fristInput/$secondInput))
 echo $result2
+result3=$(($fristInput%$secondInput+$thirdInput))
+echo $result3
+
+arithmaticOperation[result]=$result
+arithmaticOperation[result1]=$result1
+arithmaticOperation[result2]=$result2
+arithmaticOperation[result3]=$result3
+echo ${arithmaticOperation[@]}
