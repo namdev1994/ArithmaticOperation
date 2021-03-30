3896ab26a72098fa906e532dccde75317e87b53b
namdev1994 <n.lavhare1994@gmail.com>
2021-03-30 13:00:24 +0530

    Added:Arithmatic operation using Array

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -3,17 +3,23 @@ declare -A arithmaticOperation
 read -p "Enter the frist input :" fristInput
 read -p "Enter the second input :" secondInput
 read -p "Enter the third input :" thirdInput
-result=$(($fristInput+$secondInput*$thirdInput))
-echo $result
-result1=$(($fristInput*$secondInput+$thirdInput))
+result1=$(($fristInput+$secondInput*$thirdInput))
 echo $result1
-result2=$(($thirdInput+$fristInput/$secondInput))
+result2=$(($fristInput*$secondInput+$thirdInput))
 echo $result2
-result3=$(($fristInput%$secondInput+$thirdInput))
+result3=$(($thirdInput+$fristInput/$secondInput))
 echo $result3
+result4=$(($fristInput%$secondInput+$thirdInput))
+echo $result4
 
-arithmaticOperation[result]=$result
 arithmaticOperation[result1]=$result1
 arithmaticOperation[result2]=$result2
 arithmaticOperation[result3]=$result3
+arithmaticOperation[result4]=$result4
 echo ${arithmaticOperation[@]}
+for((i=0; i<=${#arithmaticOperation[@]}; i++))
+do
+  array[i]=${arithmaticOperation[result$((i+1))]}
+done
+#TO PRINT
+echo "${array[@]}"
