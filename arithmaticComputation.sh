c74caa4bb51130dd3662e58df6267d297355f52c
namdev1994 <n.lavhare1994@gmail.com>
2021-03-30 11:55:23 +0530

    Added:Arithmatic operation compute

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -3,3 +3,5 @@ read -p "Enter the second input :" secondInput
 read -p "Enter the third input :" thirdInput
 result=$(($fristInput+$secondInput*$thirdInput))
 echo $result
+result1=$(($fristInput*$secondInput+$thirdInput))
+echo $result1
